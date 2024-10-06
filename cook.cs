9b7607897871a4faefbb504236693468fdc2c89e
d0rlina <160572925+d0rlina@users.noreply.github.com>
2024-10-06 17:39:07 +0700

    Update cook.cs

--- cook.cs
+++ cook.cs
@@ -17,9 +17,9 @@ namespace практика_2._0
         {
             InitializeComponent();
         }
-
+/*неизвестная кнопка*/
         private void button2_Click(object sender, EventArgs e)
-        {
+        { /*неизвестная строка*/
             listBox1.Items.Clear();
             string connectionString = "Server=localhost;Database=dorman;User ID=root;Password=D0rlina060720;";
             // SQL-запрос для получения данных
@@ -51,7 +51,7 @@ namespace практика_2._0
                 }
             }
         }
-
+/*неизвестная кнопка*/
         private void button1_Click(object sender, EventArgs e)
         {
             string connectionString = "Server=localhost;Database=dorman;User ID=root;Password=D0rlina060720;";
@@ -82,12 +82,12 @@ namespace практика_2._0
                 }
             }
         }
-
+/*неизвестная кнопка*/
         private void button3_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-
+/*неизвестная кнопка*/
         private void button4_Click(object sender, EventArgs e)
         {
             this.Hide();
